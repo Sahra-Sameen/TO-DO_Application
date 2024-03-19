4264444842af113586604fe48d4d25a1a7af7b0c
Sahra-Sameen <126109790+Sahra-Sameen@users.noreply.github.com>
2024-03-19 20:37:01 +0700

    Completed the backend

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:random_string/random_string.dart';
+import 'package:todo_app/db_services/database.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +13,67 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   bool personal = true, college = false, office = false;
   bool suggest = false;
+  TextEditingController todoController = TextEditingController();
+  Stream? todoStream;
+
+  getonTheLoad() async {
+    todoStream = await DatabaseService().getTask(personal
+        ? "Personal"
+        : college
+            ? "College"
+            : "Office");
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget getWork() {
+    return StreamBuilder(
+        stream: todoStream,
+        builder: (context, AsyncSnapshot snapshot) {
+          return snapshot.hasData
+              ? Expanded(
+                  child: ListView.builder(
+                    itemCount: snapshot.data.docs.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot docSnap = snapshot.data.docs[index];
+                      return CheckboxListTile(
+                        activeColor: Colors.purple.shade200,
+                        title: Text(
+                          docSnap["work"],
+                        ),
+                        value: docSnap['yes'],
+                        onChanged: (newValue) async {
+                          await DatabaseService().tickMethod(
+                              docSnap["Id"],
+                              personal
+                                  ? "Personal"
+                                  : college
+                                      ? "College"
+                                      : "Office");
+                          setState(() {
+                            Future.delayed(const Duration(seconds: 2), () {
+                              DatabaseService().removeMethod(
+                                  docSnap["Id"],
+                                  personal
+                                      ? "Personal"
+                                      : college
+                                          ? "College"
+                                          : "Office");
+                            });
+                          });
+                        },
+                        controlAffinity: ListTileControlAffinity.leading,
+                      );
+                    },
+                  ),
+                )
+              : const Center(child: CircularProgressIndicator());
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +88,9 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         child: FloatingActionButton(
           backgroundColor: Colors.pink.shade200,
-          onPressed: () {},
+          onPressed: () {
+            openBox();
+          },
           child: const Icon(
             Icons.add,
             color: Colors.white,
@@ -115,10 +181,11 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       )
                     : GestureDetector(
-                        onTap: () {
+                        onTap: () async {
                           personal = true;
                           college = false;
                           office = false;
+                          await getonTheLoad();
                           setState(() {});
                         },
                         child: const Text("Personal",
@@ -150,10 +217,11 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       )
                     : GestureDetector(
-                        onTap: () {
+                        onTap: () async {
                           personal = false;
                           college = true;
                           office = false;
+                          await getonTheLoad();
                           setState(() {});
                         },
                         child: const Text("College",
@@ -185,10 +253,11 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       )
                     : GestureDetector(
-                        onTap: () {
+                        onTap: () async {
                           personal = false;
                           college = false;
                           office = true;
+                          await getonTheLoad();
                           setState(() {});
                         },
                         child: const Text("Office",
@@ -198,35 +267,106 @@ class _HomeScreenState extends State<HomeScreen> {
             const SizedBox(
               height: 20,
             ),
-            CheckboxListTile(
-              activeColor: Colors.purple.shade200,
-              title: const Text(
-                "Practice Coding for atleast 2 Hours !",
-              ),
-              value: suggest,
-              onChanged: (newValue) {
-                setState(() {
-                  suggest = newValue!;
-                });
-              },
-              controlAffinity: ListTileControlAffinity.leading,
-            ),
-            CheckboxListTile(
-              activeColor: Colors.purple.shade200,
-              title: const Text(
-                "Go to Lectures !",
-              ),
-              value: suggest,
-              onChanged: (newValue) {
-                setState(() {
-                  suggest = newValue!;
-                });
-              },
-              controlAffinity: ListTileControlAffinity.leading,
-            ),
+            getWork(),
           ],
         ),
       ),
     );
+  }
+
+  Future openBox() {
+    return showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: SingleChildScrollView(
+                child: Container(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Icon(Icons.cancel),
+                            ),
+                            const SizedBox(
+                              height: 60.0,
+                            ),
+                            Text(
+                              'Add ToDo Activity ~',
+                              style: TextStyle(
+                                color: Colors.purple.shade400,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        const Text("Add Text"),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10.0,
+                          ),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            color: Colors.black,
+                            width: 2.0,
+                          )),
+                          child: TextField(
+                            controller: todoController,
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Enter the Activity...",
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        Center(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.purple.shade200,
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            width: 100,
+                            padding: const EdgeInsets.all(5),
+                            child: GestureDetector(
+                              onTap: () {
+                                String id = randomAlphaNumeric(10);
+                                Map<String, dynamic> userTodo = {
+                                  "work": todoController.text,
+                                  "Id": id,
+                                  "Yes": false,
+                                };
+                                personal
+                                    ? DatabaseService()
+                                        .addPersonalTask(userTodo, id)
+                                    : college
+                                        ? DatabaseService()
+                                            .addCollegeTask(userTodo, id)
+                                        : DatabaseService()
+                                            .addOfficeTask(userTodo, id);
+                                Navigator.pop(context);
+                              },
+                              child: const Center(
+                                child: Text(
+                                  "Add",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ]),
+                ),
+              ),
+            ));
   }
 }
